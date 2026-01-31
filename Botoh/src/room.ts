3e04b2f0a968338ee8443e135fea4f51f2d10060
Ximb <senhorximbinha@gmail.com>
2026-01-31 14:55:39 -0300

    feat: portero and avatar null

--- Botoh/src/room.ts
+++ Botoh/src/room.ts
@@ -57,7 +57,7 @@ export const roomPromise: Promise<any> = HaxballJS().then((HBInit: any) => {
     maxPlayers: maxPlayers,
     password: roomPassword ?? undefined,
     token:
-      process.env.HAXBALL_TOKEN ?? "thr1.AAAAAGl80he7aNFpTI7t3A._8PDzE1Y9nM",
+      process.env.HAXBALL_TOKEN ?? "thr1.AAAAAGl-PmrECM7JvgFmjA.DmjBDTw0rB0",
     geo: getGeo(),
   });
 
