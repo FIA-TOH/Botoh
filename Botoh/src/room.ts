5c307ab7e38c541d6147f710c7137a42de476011
Nanoseb <Nanoseb33@gmail.com>
2026-06-13 19:56:36 -0400

    cambios en room.ts

--- Botoh/src/room.ts
+++ Botoh/src/room.ts
@@ -60,7 +60,7 @@ export const roomPromise: Promise<any> = HaxballJS().then((HBInit: any) => {
     maxPlayers: maxPlayers,
     password: roomPassword ?? undefined,
     token:
-      process.env.HAXBALL_TOKEN ?? "thr1.AAAAAGot37u2fFmDR-mhdA.KGxq8FQ_rQY",
+      process.env.HAXBALL_TOKEN ?? "thr1.AAAAAGn6WnSY1rFLI_fR6A.KyDGOSCUBxo",
     geo: getGeo(),
   });
 
